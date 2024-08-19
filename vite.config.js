240ed233158bdac5b813a8670e4ff04ef1658c0b
TimosLeBoss <tatibatchi15@hotmail.com>
2024-08-19 22:40:01 +0100

    Bien joue Negro

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ksm/",
+  base: '/ksm/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
